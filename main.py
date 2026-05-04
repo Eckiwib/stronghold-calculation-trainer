8af67befa5dc4cbdf69c39c3e35562638e7f2585
Eckiwib <eckhardwib@outlook.de>
2026-05-04 23:09:09 +0200

    main part finished

--- main.py
+++ main.py
@@ -6,17 +6,29 @@ import json
 with open("settings.json","r") as file:
     settings = json.load(file)
 
+def distance(difference):
+    return round(20 / math.cos(math.radians(90-difference)))
+
 inp = 4
 
 while True:
-    difference_range = random.choice([True, False])
-    if difference_range:
-        difference = random.randint(38, 115) / 100
-    else:
-        difference = random.randint(114, 300) / 100
-    
+    operator = random.choice([True, False])
+    difference = random.randint(38, 300) / 100
     angle_1 = random.randint(-18000, 18000) / 100
-    angle_2 = angle_1 - difference
+    if operator:
+        angle_2 = round(angle_1 + difference, 2)
+    else:
+        angle_2 = round(angle_1 - difference, 2)
+
     while inp != difference:
-        print(f"Angle 1 = {angle_1} Angle 2 = {angle_2}")
-        inp = float(input())
+        print()
+        print(angle_1)
+        print(angle_2)
+        inp = float(input("Difference = "))
+
+    print("Correct")
+    inp = -1
+    while inp != distance(difference=difference):
+        inp = float(input("Distance = "))
+    
+    print("Correct")
